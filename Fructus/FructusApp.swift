336deb630ea1898e9ef243069618a411b686a4e2
Dmitry V <dmitryjamesver@icloud.com>
2022-02-17 13:53:37 -0700

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Dmitry Ver on 2/17/22.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
